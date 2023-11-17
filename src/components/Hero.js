00c9028a4619c48b328e6d0d8bf1b7d45e5c6bab
cybertronbot <okhuomonajayi54@gmail.com>
2023-11-17 18:29:14 +0100

    commit

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,16 +7,16 @@ import NewCollection from "./NewCollection";
 
 function Hero() {
   return (
-    <div className="h-screen">
+    <div className="xl:h-screen">
       <Navbar />
       <div className="">
         <div className="">
-          <div className="flex justify-between items-center border-t-[1px] border-b-[1px] v relative  border-[#fff]  px-14 w-full">
+          <div className="flex justify-between xl:flex-row flex-col items-center xl:gap-0 gap-5 border-t-[1px] border-b-[1px] sm:v relative  border-[#fff] px-4  xl:px-14 w-full">
             <div className="flex flex-1 flex-col  border-[#fff]">
-              <h1 className="text-white otf text-[97px] leading-[110px]">
-                Virtual world
+              <h1 className="text-white otf xl:text-[97px] text-[40px] leading-[50px] xl:leading-[110px] xl:mt-0 mt-14">
+                Virtual <br/> world
               </h1>
-              <h4 className="monument-ultra text-[#A3A3A3] xl:w-[708px]">
+              <h4 className="monument-ultra text-[#A3A3A3] xl:text-base text-sm xl:w-[708px]">
                 WELCOME TO Monaki! MEET OUR COLLECTION OF 3.333 UNIQUE NFT.
                 SUPPORT AND JOIN OUR COMMUNITY, A NEW WORLD ARE WAITING FOR YOU.
               </h4>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,18 +3,18 @@ import React from "react";
 function Navbar() {
   return (
     <div>
-      <div className="px-10 h-[77px] flex justify-between items-center ">
+      <div className="xl:px-10 px-4 xl:h-[77px] h-[60px] flex justify-between items-center ">
         <div>
           <h5 className="text-white text-sm monument-normal">Monaki</h5>
         </div>
-        <nav>
+        <nav className="xl:flex hidden">
           <ul className="flex gap-[54px] items-center text-white text-sm monument-normal">
             <li>HOME</li>
             <li>GALLERY</li>
             <li>DISCORD</li>
           </ul>
         </nav>
-        <button className="px-16 py-3 bg-[#FFF] rounded-[120px] shadow-[4px_4px_0_0_#7843E8] text-black text-sm monument-normal">BUY NFT</button>
+        <button className="xl:px-16 px-8 xl:py-3 py-2 bg-[#FFF] rounded-[120px] shadow-[4px_4px_0_0_#7843E8] text-black text-sm monument-normal">BUY NFT</button>
       </div>
     </div>
   );
